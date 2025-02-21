f924c6be8eeafc018d75a9234e54d3ea06ab8779
Benjamin Chait <3802865+benjaminchait@users.noreply.github.com>
2025-02-21 15:36:37 -0600

    Initial Commit

---
+++ BenjaminBluesky/BenjaminBlueskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenjaminBlueskyApp.swift
+//  BenjaminBluesky
+//
+//  Created by Benjamin Chait on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BenjaminBlueskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BenjaminBlueskyTests/BenjaminBlueskyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BenjaminBlueskyTests.swift
+//  BenjaminBlueskyTests
+//
+//  Created by Benjamin Chait on 2/21/25.
+//
+
+import Testing
+@testable import BenjaminBluesky
+
+struct BenjaminBlueskyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
